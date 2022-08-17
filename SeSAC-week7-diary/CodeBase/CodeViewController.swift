21ee9f62f6c1a434a1294c1fef9586ad26055d7b
jiun <vonreal20@gmail.com>
2022-08-17 22:42:33 +0900

    🌱 [0817 - 수업] 이전까지 레이아웃을 코드로 관리하는 것들 실습

--- SeSAC-week7-diary/CodeBase/CodeViewController.swift
+++ SeSAC-week7-diary/CodeBase/CodeViewController.swift
@@ -11,22 +11,68 @@ import UIKit
     공통
     1. 뷰객체 프로퍼티 선언. 클래스의 인스턴스를 생성
     2. 명시적으로 루트뷰에 추가
-    3. 크기와 위치 및 속성 정의 -> Frame 기반 한계
+    3. 크기와 위치 및 속성 정의
+        => Frame 기반 한계
+        => AutoResizingMask, AutoLayout 등장
+        => NSLayoutConstraints
  */
 
 class CodeViewController: UIViewController {
 
     
     let emailTextField = UITextField()
-    
+    let passwordTextField = UITextField()
+    let signButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signButton)
         
-        emailTextField.frame = CGRect(x: 50, y: 150, width: UIScreen.main.bounds.width - 100, height: 50)
+        // [1] Frame 기반
+        emailTextField.frame = CGRect(x: 50, y: 50, width: UIScreen.main.bounds.width - 100, height: 50)
         emailTextField.borderStyle = .line
         emailTextField.backgroundColor = .lightGray
+        
+        // [2] NSLayoutConstraints 기반 -> iOS 6+
+        // -> AutoResizingMask와 충돌이 일어날 수 있으니 하나는 꺼줘야 한다.
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.borderStyle = .line
+        passwordTextField.backgroundColor = .lightGray
+//
+//        let top = NSLayoutConstraint(item: passwordTextField, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 100)
+//        top.isActive = true // 레이아웃 활성화
+//
+//        NSLayoutConstraint(item: passwordTextField, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 50).isActive = true
+//
+//        NSLayoutConstraint(item: passwordTextField, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -50).isActive = true
+//
+//        NSLayoutConstraint(item: passwordTextField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50).isActive = true
+    
+        
+        // [3] NSLayoutConstraints addConstraints
+        let top = NSLayoutConstraint(item: passwordTextField, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 100)
+        
+        let leading = NSLayoutConstraint(item: passwordTextField, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 50)
+        
+        let trailing = NSLayoutConstraint(item: passwordTextField, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -50)
+        
+        let height = NSLayoutConstraint(item: passwordTextField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 50)
+        
+        view.addConstraints([top, leading, trailing, height])
+        
+        // [4] NSLayoutAnchor -> iOS 9+
+        signButton.translatesAutoresizingMaskIntoConstraints = false
+        signButton.backgroundColor = .red
+        
+        NSLayoutConstraint.activate([
+            signButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signButton.widthAnchor.constraint(equalToConstant: 300),
+            signButton.heightAnchor.constraint(equalToConstant: 50),
+            signButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
+    
 }
